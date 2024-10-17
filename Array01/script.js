82bd097cc4a1cddc00fe7e1b4dfd2a409a1be4d9
Caioh-12 <silva.caioh08@gmail.com>
2024-10-17 11:26:08 -0300

    array

---
+++ Array01/script.js
@@ -0,0 +1,16 @@
+//variaveis unitarias 
+const name = 'Giovana'
+const name2 = 'Natalia'
+const name3 = 'Thalita'
+
+
+
+//variaveis de arrays 
+
+const nomes = ['Giovana', 'Natalia', 'Thalita']
+console.log(nomes)
+
+//imprimindo os valores do array
+console.log(nomes[2])
+nomes[10] = 'luiz'
+console.log(nomes[10])
