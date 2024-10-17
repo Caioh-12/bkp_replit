ca707c55c186b795245e366cd2b2675ef4a602c4
Caioh-12 <silva.caioh08@gmail.com>
2024-10-17 11:26:47 -0300

    revisão2tri

---
+++ revisao2tri/js_mobile/funcoes.js
@@ -0,0 +1,12 @@
+/**
+ *Funções são blocos que podem ser reutilizados
+ * funções podem ou não receber parametros
+ * funções podem ou não retornar valores
+ * funções podem ser anonimas
+ */
+// Declaração de função
+function dizOla() {
+  alert('Olá')
+}
+// invocação da função
+dizOla()
---
+++ revisao2tri/variaveis/script.js
@@ -0,0 +1,7 @@
+const comidas = document.querySelector('.comidas')
+
+const comida1 = prompt('Digite sua comida n° 1')
+
+const comida2 = prompt('Digite sua comida n° 2')
+
+comidas.innertext = comida1;
