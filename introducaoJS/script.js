67dff2bbb2cacb14894d7ac01b4dd1a674307ded
Caioh-12 <silva.caioh08@gmail.com>
2024-10-18 09:22:03 -0300

    introduçãojs

---
+++ introducaoJS/script.js
@@ -0,0 +1,11 @@
+
+// crie uma variavel para cada um dos dados listados (nome,sobrenome,telefone,estacasado)imprima cada um os valores com console.log(variavel))
+const nome = "Natalia"
+const sobrenome = "Carrah"
+const telefone = "41-987654321"
+const estacasado = true
+
+console.log(nome)
+console.log(sobrenome)
+console.log(telefone)
+console.log(estacasado)
